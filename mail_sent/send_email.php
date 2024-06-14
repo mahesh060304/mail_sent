aaf93f92bbf61b25d531241dac2baad3d162a890
mahesh060304 <mahesh.k2021cseb@sece.ac.in>
2024-06-14 12:16:44 +0530

    env

--- mail_sent/send_email.php
+++ mail_sent/send_email.php
@@ -16,8 +16,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'kmahesh060304@gmail.com'; 
-        $mail->Password = 'ruji oowm sbzu cope'; 
+        $mail->Username = $mail_username; 
+        $mail->Password = $mail_password; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
